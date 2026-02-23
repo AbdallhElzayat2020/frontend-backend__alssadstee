65459f413054f65763e45ccadb8de4b71c2c28a0
Abdallh ELzayat <abdallhelzayat.dev@gmail.com>
2026-02-23 15:19:41 +0200

    Refactor head layout for improved code readability and formatting consistency

--- resources/views/website/layouts/head.blade.php
+++ resources/views/website/layouts/head.blade.php
@@ -5,13 +5,13 @@
     <title>
         @yield('title')
     </title>
-    <link rel="icon" href="{{asset('assets/website/images/favicon.png')}}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('assets/website/images/favicon.png') }}" type="image/x-icon">
     @php
-$currentLocale = LaravelLocalization::getCurrentLocale();
-$isRTL = $currentLocale === 'ar';
-$bootstrapCSS = $isRTL
-    ? 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css'
-    : 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css';
+        $currentLocale = LaravelLocalization::getCurrentLocale();
+        $isRTL = $currentLocale === 'ar';
+        $bootstrapCSS = $isRTL
+            ? 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css'
+            : 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css';
     @endphp
     <link href="{{ $bootstrapCSS }}" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
@@ -19,5 +19,5 @@ $bootstrapCSS = $isRTL
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css" />
-    <link rel="stylesheet" href="{{asset('assets/website/css/style.css')}}" />
+    <link rel="stylesheet" href="{{ asset('assets/website/css/style.css') }}" />
 </head>
